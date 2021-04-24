a795cdb7a2e6258afeca9755c91bb237265b60a5
Mia <miamorticia@Mias-MBP.lan>
2021-04-24 17:55:07 -0600

    add styled components

--- src/App/index.js
+++ src/App/index.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
+import WelcomeMessage from './WelcomeMessage'
+import styled from 'styled-components'
+
+
 
 
 class App extends Component {
 render() {
   return (
-<div>Hello Martians</div>
-  );
+    <div>
+      <WelcomeMessage />
+    </div>
+    );
   }
 }
 export default App;
