8d5fbaddecf6fc2448f3f05245fdc3a665c94094
Mia <miamorticia@Mias-MBP.lan>
2021-04-25 20:15:46 -0600

    style app bar

--- src/App/AppBar.js
+++ src/App/AppBar.js
@@ -1,17 +1,43 @@
 import React from 'react';
-import styled, {css} from 'styled-components';
+import styled, { css } from 'styled-components';
+
+const Logo = styled.div`
+    font-size:1.5em;
+`
 
 const Bar = styled.div`
     display: grid; 
-    /* margin-bottom: 40px;  */
-    grid-template-columns: 180px auto 100px 100px; 
+    margin-bottom: 40px;
+    grid-template-columns: 180px auto 100px 100px 100px; 
 `
 
+const ControlButtonElem = styled.div`
+    cursor: pointer; 
+    ${props => props.active && css`
+        text-shadow: 0px 0px 60px #03ff03;
+    `}
+`
+
+function toProperCase(lower){
+    return lower.charAt(0).toUpperCase() + lower.substr(1);
+}
+
+function ControlButton({name, active}){
+    return (
+        <ControlButtonElem active ={active}>
+            {toProperCase(name)}
+        </ControlButtonElem>
+        )
+}
+
 export default function(){
-    return <Bar>
-        <div> Marbas </div>
-        <div></div>
-        <div> Dashboard </div>
-        <div> Settings </div>
+    return (
+    <Bar>
+        <Logo> Marbas </Logo>
+        <div />
+        <ControlButton active name="dashboard" />
+        <ControlButton name="settings" />
+        <ControlButton name="about" />
     </Bar>
+    );
 }
