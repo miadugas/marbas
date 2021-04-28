7ef2ef8efe418ade5ef76e5c467e39c58704e7b9
Mia <miamorticia@Mias-MacBook-Pro.local>
2021-04-27 21:47:33 -0600

    added coin grid

---
+++ src/Settings/CoinGrid.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import styled from 'styled-components';
+import {AppContext} from "../App/AppProvider";
+// import CoinTile from "./CoinTile";
+
+export const CoinGridStyled = styled.div`
+    display: grid;   
+    grid-template-columns: repeat(auto-fill, minmax(130px, 1fr)); 
+    /* grid-gap: 15px; 
+    margin-top: 40px;  */
+`
+
+// function getLowerSectionCoins(coinList, filteredCoins){
+//   return (filteredCoins && Object.keys(filteredCoins)) ||
+//     Object.keys(coinList).slice(0, 100)
+// }
+
+// function getCoinsToDisplay(coinList, topSection, favorites, filterCoins){
+//   return topSection ? favorites : getLowerSectionCoins(coinList, filterCoins);
+// }
+
+export default function (){
+    return <AppContext.Consumer>
+    {({
+    coinList 
+    // favorites, 
+    // filteredCoins
+}) => <CoinGridStyled>
+            {Object.keys(coinList).map(coinKey =>
+                <div> {coinKey}</div>
+                )}
+                </CoinGridStyled>}
+    </AppContext.Consumer>
+
+}
+
