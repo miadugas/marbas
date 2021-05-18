697e5f5f74411cd37eed4709c5b9cd5b099be3d8
Mia <miamorticia@Mias-MacBook-Pro.local>
2021-05-17 18:21:19 -0600

    under the hood logic

--- src/Settings/CoinGrid.js
+++ src/Settings/CoinGrid.js
@@ -1,32 +1,34 @@
 import React from 'react';
 import styled from 'styled-components';
 import {AppContext} from "../App/AppProvider";
-import {SelectableTile} from "../Shared/Tile";
+import CoinTile from "./CoinTile";
 
 export const CoinGridStyled = styled.div`
-    display: grid;   
-    grid-template-columns: repeat(auto-fill, minmax(130px, 1fr)); 
-    grid-gap: 15px; 
-    /* margin-top: 40px;  */
+  display: grid;   
+  grid-template-columns: repeat(auto-fill, minmax(130px, 1fr)); 
+  grid-gap: 15px; 
+  margin-top: 40px; 
 `
 
-// function getLowerSectionCoins(coinList, filteredCoins){
-//   return (filteredCoins && Object.keys(filteredCoins)) ||
-//     Object.keys(coinList).slice(0, 100)
-// }
-
-function getCoinsToDisplay(coinList){
-    return Object.keys(coinList).slice(0, 100);
-    // topSection ? favorites : getLowerSectionCoins(coinList, filterCoins);
+function getLowerSectionCoins(coinList, filteredCoins){
+  return (filteredCoins && Object.keys(filteredCoins)) ||
+    Object.keys(coinList).slice(0, 100)
 }
 
-export default function (){
-    return <AppContext.Consumer>
-        {({coinList}) => <CoinGridStyled>
-            {Object.keys(coinList).map(coinKey =>
-            <SelectableTile> {coinKey} </SelectableTile> 
-            )}
-        </CoinGridStyled>}
-    </AppContext.Consumer>
-    }
+function getCoinsToDisplay(coinList, topSection, favorites, filterCoins){
+  return topSection ? favorites : getLowerSectionCoins(coinList, filterCoins);
+}
 
+export default function ({topSection}){
+  return (
+    <AppContext.Consumer>
+    {({coinList, favorites, filteredCoins}) => (
+      <CoinGridStyled>
+        {getCoinsToDisplay(coinList, topSection, favorites, filteredCoins).map(coinKey =>
+          <CoinTile key={coinKey} topSection={topSection} coinKey={coinKey}/>
+        )}
+      </CoinGridStyled>
+    )}
+  </AppContext.Consumer>
+  );
+}
--- src/Settings/CoinTile.js
+++ src/Settings/CoinTile.js
@@ -4,35 +4,35 @@ import {SelectableTile, DisabledTile, DeletableTile} from "../Shared/Tile";
 import CoinHeaderGrid from './CoinHeaderGrid';
 import CoinImage from '../Shared/CoinImage';
 
-// function clickCoinHandler(topSection, coinKey, addCoin, removeCoin){
-//     return topSection ? () => {
-//     removeCoin(coinKey)
-//     } : () => {
-//     addCoin(coinKey)
-//     }
-// }
+function clickCoinHandler(topSection, coinKey, addCoin, removeCoin){
+  return topSection ? () => {
+    removeCoin(coinKey)
+  } : () => {
+    addCoin(coinKey)
+  }
+}
 
 export default function({coinKey, topSection}){
-    return <AppContext.Consumer>
+  return <AppContext.Consumer>
     {({coinList, addCoin, removeCoin, isInFavorites}) => {
-        let coin = coinList[coinKey];
+      let coin = coinList[coinKey];
 
-        let TileClass = SelectableTile;
-        if(topSection){
+      let TileClass = SelectableTile;
+      if(topSection){
         TileClass = DeletableTile;
-        }else if(isInFavorites(coinKey)){
+      }else if(isInFavorites(coinKey)){
         TileClass = DisabledTile;
-        }
+      }
 
-        return <TileClass
+      return <TileClass
         onClick={clickCoinHandler(topSection, coinKey, addCoin, removeCoin)}
-        >
+      >
         <CoinHeaderGrid
-            topSection={topSection}
-            name={coin.CoinName}
-            symbol={coin.Symbol}/>
+          topSection={topSection}
+          name={coin.CoinName}
+          symbol={coin.Symbol}/>
         <CoinImage coin={coin}/>
-        </TileClass>
+      </TileClass>
     }}
-    </AppContext.Consumer>
+  </AppContext.Consumer>
 }
